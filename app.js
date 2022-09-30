d69aae07d61381a991e6aa34e76ca975aa29c284
dinhanhduy1996 <96035586+dinhanhduy1996@users.noreply.github.com>
2022-09-30 13:10:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-const server = require('http').createServer(app);
-
-/* kết nối database */
-
-
-/*  */
-/* database tiep tuc */
-
-const PORT = 5000;
-server.listen(process.env.PORT||PORT,()=>{
-    console.log('server dang chay');
+var express = require('express');
+var app = express();
+ 
+app.get('/', function(req, res){
+   res.send("Hello world!");
 });
+ 
+app.listen(3000);
