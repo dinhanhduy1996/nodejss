af302d3a20384f402cbc58b8d3eed8dcd927362d
dinhanhduy1996 <96035586+dinhanhduy1996@users.noreply.github.com>
2022-09-30 13:05:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,39 +2,13 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {origin: "*"}
-});
+
 /* kết nối database */
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database:"laravel"
-  });
+
 
 /*  */
 /* database tiep tuc */
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-/*  */
-    io.on('connection',(socket)=>{
-        /* phấn của socket io, socket.on là nhận, io.mit là gởi */
-            socket.on('chat message', function(msg){
-            io.emit('chat message',msg);
-            console.log(msg);
-        /*  */
-        /* thêm vào database */
-            var sql = "INSERT INTO chatting (noidung) VALUES ("+'"'+msg+'"'+")";
-            con.query(sql, function (err, result) {
-              if (err) throw err;
-              console.log("da them vao database");
-            });
-        /*  */
-            });
-        });
-    });
+
 const PORT = 5000;
 server.listen(process.env.PORT||PORT,()=>{
     console.log('server dang chay');
